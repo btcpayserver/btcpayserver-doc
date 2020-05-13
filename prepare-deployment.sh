b40e529c3099d8af741ddd894a2c0d4fbfcb18df
Dennis Reimann <mail@dennisreimann.de>
2020-05-13 10:46:27 +0200

    Setup redirects for GH Pages

---
+++ prepare-deployment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+# https://github.blog/2009-12-29-bypassing-jekyll-on-github-pages/
+touch docs/.vuepress/dist/.nojekyll
+
+# https://help.github.com/en/articles/managing-a-custom-domain-for-your-github-pages-site
+# echo docs.btcpayserver.org > docs/.vuepress/dist/CNAME
+echo btcpaydocs.uix.space > docs/.vuepress/dist/CNAME
+
+# Set up redirects
+node create-redirects.js
