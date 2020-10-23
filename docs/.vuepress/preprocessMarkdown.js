3897ee908f188338c9a0bfe311fdcd49227583e9
Dennis Reimann <mail@dennisreimann.de>
2020-10-23 09:22:28 +0200

    Fix replaceExternalRepoLinks and improve logging

--- docs/.vuepress/preprocessMarkdown.js
+++ docs/.vuepress/preprocessMarkdown.js
@@ -1,5 +1,6 @@
 const { join, relative, resolve } = require('path')
 const docsDir = resolve(__dirname, '..')
+const isDev = process.env.NODE_ENV === 'development'
 
 // external docs: local dir as key, repo base as value
 const EXTERNAL_DOCS = {
@@ -8,24 +9,35 @@ const EXTERNAL_DOCS = {
   'Transmuter': 'https://github.com/btcpayserver/btcTransmuter',
 }
 
-const replaceExternalRepoLinks = (source, resourcePath) => {
+const replaceExternalRepoLinks = (source, externalRepoUrl, resourcePath) => {
+  const replacements = []
+  const replace = (text, str, replacement, type) => (replacements.push([str, replacement, type]), text.replace(str, replacement))
+
   let processed = source
 
   Object.keys(EXTERNAL_DOCS).forEach(baseDir => {
     const baseUrl = EXTERNAL_DOCS[baseDir]
 
-    // rewrite repo internal links to external links
-    const repoLinks = new RegExp(`\\]\\(((?!https?:\/\/|#|\.?\/).*?)\\)`, 'gi')
-    processed = processed.replace(repoLinks, (all, path) => all.replace(path, `${baseUrl}/blob/master/${path}`))
+    if (baseUrl === externalRepoUrl) {
+      // rewrite repo internal links to external links
+      const repoLinks = new RegExp(`\\]\\(((?!https?:\/\/|#|\.?\/).*?)\\)`, 'gi')
+      processed = processed.replace(repoLinks, (all, path) => replace(all, path, `${baseUrl}/blob/master/${path}`, 1))
 
-    // rewrite links to docs to internal VuePress links
-    const docsLinks = new RegExp(`\\]\\((${baseUrl}/blob/master/((README\.md|docs/).*?))\\)`, 'gi')
-    processed = processed.replace(docsLinks, (all, url, path) => all.replace(url, `/${baseDir}/${path.replace('docs/', '')}`))
+      // rewrite links to docs to internal VuePress links
+      const docsLinks = new RegExp(`\\]\\((${baseUrl}/blob/master/((README\.md|docs/).*?))\\)`, 'gi')
+      processed = processed.replace(docsLinks, (all, url, path) => replace(all, url, `/${baseDir}/${path.replace('docs/', '')}`, 2))
+    }
 
     // rewrite external links to docs to internal VuePress links
     const links = new RegExp(`\\]\\((https://docs.btcpayserver.org(.*))\\)`, 'gi')
-    processed = processed.replace(links, (all, url, path) => all.replace(url, path))
+    processed = processed.replace(links, (all, url, path) => replace(all, url, path, 3))
   })
+
+  if (replacements.length && !isDev) {
+    console.group('Markdown replacements in', resourcePath)
+    replacements.forEach(([str, rep, type]) => console.log(`${type}:`, str, '->', rep))
+    console.groupEnd()
+  }
 
   return processed
 }
@@ -60,11 +72,11 @@ const replaceYouTubeLinks = source =>
 module.exports = function (source) {
   const { resourcePath } = this
   let processed = source
-
   processed = replaceYouTubeLinks(processed)
 
-  if (source.match(`externalRepo: `)) {
-    processed = replaceExternalRepoLinks(processed, resourcePath)
+  const [, externalRepo] = source.match(/externalRepo: (.*)/) || []
+  if (externalRepo) {
+    processed = replaceExternalRepoLinks(processed, externalRepo, resourcePath)
   }
 
   return processed
