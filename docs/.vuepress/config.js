948ba66fcafe23f5025fd020537c24589f601454
Dennis Reimann <mail@dennisreimann.de>
2021-03-22 11:16:47 +0100

    Remove Safari mask icon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,10 +22,7 @@ module.exports = {
   head: [
     // Favicon
     ["link", { rel: "apple-touch-icon", href: "/apple-touch-icon.png", sizes: "180x180" }],
-    ["link", { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" }],
-    ["link", { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" }],
     ["link", { rel: "manifest", href: "/site.webmanifest" }],
-    ["link", { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#51b13e" }],
     ["meta", { name: "msapplication-TileColor", content: "#0f3b21" }],
     ["meta", { name: "theme-color", content: "#ffffff" }],
 
