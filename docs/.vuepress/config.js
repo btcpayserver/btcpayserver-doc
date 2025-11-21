2f2fca7fbbc8835d0cbfdab7a3ba187b5fbda127
ndeet <andy.tasch@gmail.com>
2025-11-21 10:14:08 +0100

    Update docs/.vuepress/config.js
    Co-authored-by: Pavlenex <pavlenex@icloud.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -112,7 +112,7 @@ const sidebarUserGuide = [
     ['/BigCommerce/', 'Big Commerce'],
     ['/Calcom', 'Cal.com'],
     ['/Drupal/', 'Drupal'],
-    ['/easywebshop', 'easywebshop'],
+    ['/easywebshop', 'EasyWebshop'],
     ['/Ecwid/', 'Ecwid'],
     ['/GiveWP', 'GiveWP'],
     ['/Grandnode/', 'Grandnode'],
